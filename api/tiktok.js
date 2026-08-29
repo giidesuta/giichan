3167f274220b1b8e67b6a9b53040c0e712bba55e
giidesuta <sobatinfo6404@gmail.com>
2026-08-29 11:10:18 +0700

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -116,7 +116,7 @@ function createDownloadUrl(url, filename) {
 
   const token = createToken(url, filename);
 
-  return "/api/tiktok?download=" + encodeURIComponent(token);
+  return "giichan/api/tiktok?download=" + encodeURIComponent(token);
 }
 
 async function getTikTokData(tiktokUrl) {
